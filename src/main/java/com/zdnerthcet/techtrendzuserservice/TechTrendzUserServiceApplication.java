a3354f20a0e8e964b2b6be5495280502061ecf64
Gabriele Grosse <56041468+perfect-droidman@users.noreply.github.com>
2024-03-12 11:53:04 -0400

    Initial commit

---
+++ src/main/java/com/zdnerthcet/techtrendzuserservice/TechTrendzUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zdnerthcet.techtrendzuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTrendzUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTrendzUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zdnerthcet/techtrendzuserservice/TechTrendzUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdnerthcet.techtrendzuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTrendzUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
